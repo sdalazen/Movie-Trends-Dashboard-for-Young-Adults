75037a66aec07368afb382a97405c74036e2f0a6
sdalazen <sba22149@student.cct.ie>
2025-05-23 16:56:37 +0100

    Start

--- Movie-Trends-Dashboard-for-Young-Adults.py
+++ Movie-Trends-Dashboard-for-Young-Adults.py
@@ -4,6 +4,7 @@ import numpy as np
 
 st.title("Movie Trends Dashboard for Young Adults (18–35)")
 
+@st.cache_data
 def load_data():
     ratings = pd.read_csv('ratings.csv')
     movies = pd.read_csv('movies.csv', encoding='ISO-8859-1')
