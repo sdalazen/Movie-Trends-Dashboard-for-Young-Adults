700ce1ac04ac2ccd09fc6e740702a605f021fde2
sdalazen <sba22149@student.cct.ie>
2025-05-23 18:38:30 +0100

    Start

--- Movie-Trends-Dashboard-for-Young-Adults.py
+++ Movie-Trends-Dashboard-for-Young-Adults.py
@@ -7,15 +7,6 @@ from wordcloud import WordCloud
 
 st.title("Movie Trends Dashboard for Young Adults (18–35)")
 
-@st.cache_data
-def load_data():
-    ratings = pd.read_csv('ratings.csv')
-    movies = pd.read_csv('movies.csv', encoding='ISO-8859-1')
-    tags = pd.read_csv('tags.csv', encoding='ISO-8859-1')
-    return ratings, movies, tags
-
-ratings, movies, tags = load_data()
-
 st.write("Ratings columns:", ratings.columns.tolist())
 st.write("Movies columns:", movies.columns.tolist())
 st.write("Tags columns:", tags.columns.tolist())
